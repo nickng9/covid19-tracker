4acbe0f81c3d281c9354be8380ca414dddde45c2
Nicholas Ng <nicholas.m.ng@gmail.com>
2020-12-10 20:45:42 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+// npm create-react-app ./
+// npm start for running
+// npm install --save axios 
+// npm install --save react-chartjs-2
+// npm install --save axios 
+// npm install --save react-countup
+// npm install --save classnames
+// npm install --save @material-ui/core
+
 import React from 'react';
 
 import ReactDOM from 'react-dom';
